3ea7eb26d8523db4382c8f7c634e23b3bc75e357
kawai10 <77530278+kawai10@users.noreply.github.com>
2021-03-17 17:46:50 +0900

    Method

--- Java playground - IDE/HelloWorld/src/Array.java
+++ Java playground - IDE/HelloWorld/src/Array.java
@@ -1,33 +1,50 @@
 import java.util.Scanner;
 
 public class Array {
+	public static int[] calculator(int times) {
+		int[] result = new int[9];
+		for (int i = 0; i < result.length; i++) {
+	           result[i] = times * (i + 1);
+	    }
+		return result;
+	}
+	public static void print(int[] result) {
+		for (int i = 0; i < result.length; i++) {
+			System.out.println(result[i]);
+		}
+		
+	}
+	
+	
 
 	public static void main(String[] args) {
-//		int[] result = new int[9];
-//		for (int i = 0; i < result.length; i++) {
-//			result[i] = 2 * (i + 1);
-//		}
-//		
-//		for (int i = 0; i < result.length; i++) {
-//			System.out.println(result[i]);
-//		}
-		
-		
-		int[] result = new int[9];
-		System.out.println("구구단 중 출력할 단은? : ");
-		Scanner sc = new Scanner(System.in);
-		int number = sc.nextInt();
-		System.out.println("입력값 : " + number);
-		
-		if (number < 2 || number > 9) {
-			System.out.println("잘못 입력하였습니다.");
-		} else {
-			for (int j = 1; j < 10; j++) {
-				System.out.println(number * j);
-			
-			}
-		
+		for (int i = 2; i < 10; i++) {
+			int[] result = calculator(i);
+			print(result);
 		}
+
+		
+		
+		
+		
+		
+		
+		
+//		int[] result = new int[9];
+//		System.out.println("구구단 중 출력할 단은? : ");
+//		Scanner sc = new Scanner(System.in);
+//		int number = sc.nextInt();
+//		System.out.println("입력값 : " + number);
+//		
+//		if (number < 2 || number > 9) {
+//			System.out.println("잘못 입력하였습니다.");
+//		} else {
+//			for (int j = 1; j < 10; j++) {
+//				System.out.println(number * j);
+//			
+//			}
+//		
+//		}
 	}
 
 }
