7aa5f65bf214dd84e3e6e691c5d33a3e840e17a8
kawai10 <77530278+kawai10@users.noreply.github.com>
2021-03-17 14:19:20 +0900

    5단

--- Java playground - IDE/HelloWorld/src/Gugudan.java
+++ Java playground - IDE/HelloWorld/src/Gugudan.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 
 public class Gugudan {
 
@@ -30,24 +31,38 @@ public class Gugudan {
 		
 		
 		//구구단 4단
-		int result = 4*1;
-		System.out.println(result);
-		result = 4*2;
-		System.out.println(result);
-		result = 4*3;
-		System.out.println(result);
-		result = 4*4;
-		System.out.println(result);
-		result = 4*5;
-		System.out.println(result);
-		result = 4*6;
-		System.out.println(result);
-		result = 4*7;
-		System.out.println(result);
-		result = 4*8;
-		System.out.println(result);
-		result = 4*9;
-		System.out.println(result);
+//		int result = 4*1;
+//		System.out.println(result);
+//		result = 4*2;
+//		System.out.println(result);
+//		result = 4*3;
+//		System.out.println(result);
+//		result = 4*4;
+//		System.out.println(result);
+//		result = 4*5;
+//		System.out.println(result);
+//		result = 4*6;
+//		System.out.println(result);
+//		result = 4*7;
+//		System.out.println(result);
+//		result = 4*8;
+//		System.out.println(result);
+//		result = 4*9;
+//		System.out.println(result);
+
+		
+		System.out.println("구구단 중 출력할 단은? : ");
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+        System.out.println(number * 1);
+        System.out.println(number * 2);
+        System.out.println(number * 3);
+        System.out.println(number * 4);
+        System.out.println(number * 5);
+        System.out.println(number * 6);
+        System.out.println(number * 7);
+        System.out.println(number * 8);
+        System.out.println(number * 9);
 
 	}
 
