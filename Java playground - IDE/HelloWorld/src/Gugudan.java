c1b5e94e92e998759b8d5319156c96b898e91e8b
kawai10 <77530278+kawai10@users.noreply.github.com>
2021-03-17 16:40:05 +0900

    8,9 단 조건문(for문 결합)

--- Java playground - IDE/HelloWorld/src/Gugudan.java
+++ Java playground - IDE/HelloWorld/src/Gugudan.java
@@ -70,9 +70,25 @@ public class Gugudan {
 //        	i++;
 //        }
         
-        for(int i = 1; i< 10; i++) {
-        	System.out.println(7* i);
-        }
+//        for(int i = 1; i< 10; i++) {
+//        	System.out.println(7* i);
+//        }
+		System.out.println("구구단 중 출력할 단은? : ");
+		Scanner scanner = new Scanner(System.in);
+		int number = scanner.nextInt();
+		System.out.println("number :" + number);
+		
+		if (number < 2) {
+			System.out.println("2 이상 9 이하의 값을 입력 할 수 있습니다.");
+		} else if (number > 9) {
+			System.out.println("2 이상 9 이하의 값을 입력 할 수 있습니다.");
+			
+		} else {
+			for (int j= 1; j < 10; j++) {
+				System.out.println(number * j);
+			}
+		}
+		
 
 	}
 
