9539a6f1464f80469a00403488223aca0b161577
kawai10 <77530278+kawai10@users.noreply.github.com>
2021-03-17 17:00:37 +0900

    배열

---
+++ Java playground - IDE/HelloWorld/src/Array.java
@@ -0,0 +1,15 @@
+
+public class Array {
+
+	public static void main(String[] args) {
+		int[] result = new int[9];
+		for (int i = 0; i < result.length; i++) {
+			result[i] = 2 * (i + 1);
+		}
+		
+		for (int i = 0; i < result.length; i++) {
+			System.out.println(result[i]);
+		}
+	}
+
+}
