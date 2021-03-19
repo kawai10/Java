e9ce7ec763f4beda945c79bba704a47c4903d937
kawai10 <77530278+kawai10@users.noreply.github.com>
2021-03-19 13:40:37 +0900

    Number-plus

--- Java Calender/Calender/src/calender.java
+++ Java Calender/Calender/src/calender.java
@@ -1,14 +1,21 @@
-
+import java.util.*;
 public class calender {
 
 	public static void main(String[] args) {
-		System.out.println("일  월  화 수  목 금  토");
-		System.out.println("--------------------");
-		System.out.println(" 1  2  3  4  5  6  7");
-		System.out.println(" 8  9 10 11 12 13 14");
-		System.out.println("15 16 17 18 19 20 21");
-		System.out.println("22 23 24 25 26 27 28");
-
+//		System.out.println("일  월  화 수  목 금  토");
+//		System.out.println("--------------------");
+//		System.out.println(" 1  2  3  4  5  6  7");
+//		System.out.println(" 8  9 10 11 12 13 14");
+//		System.out.println("15 16 17 18 19 20 21");
+//		System.out.println("22 23 24 25 26 27 28");
+		
+		System.out.println("두 수를 입력하세요.");
+		Scanner sc = new Scanner(System.in);
+		int Number = sc.nextInt();
+		int Number2 = sc.nextInt();
+		System.out.println("두 수의 합은 " + (Number + Number2) + "입니다.");
 	}
+	
+	
 
 }
