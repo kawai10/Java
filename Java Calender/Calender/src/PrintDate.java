e011834396f2b3d712eee3c8cca0aaccae970545
kawai10 <77530278+kawai10@users.noreply.github.com>
2021-03-19 15:25:14 +0900

    Use class method

--- Java Calender/Calender/src/PrintDate.java
+++ Java Calender/Calender/src/PrintDate.java
@@ -1,16 +1,21 @@
 import java.util.Scanner;
 
 public class PrintDate {
+	
+	private static final int[] MaxDays = {31,28,31,30,31,30,31,31,30,31,30,31};
 
+	public int getMaxDaysOfMonth(int Month) {
+		return MaxDays[Month -1];
+	}
+	
 	public static void main(String[] args) {
-		int date[] = {31,28,31,30,31,30,31,31,30,31,30,31};
 		System.out.println("달을 입력하세요.");
 		Scanner sc = new Scanner(System.in);
+		PrintDate Date = new PrintDate();
 		int Month = sc.nextInt();
+		System.out.printf("%d월은 %d일까지 있습니다.\n", Month, Date.getMaxDaysOfMonth(Month));
 		
-		System.out.printf("%d월은 %d일까지 있습니다.\n", Month, date[Month -1]);
-		
-		
+	
 	}
 
 }
